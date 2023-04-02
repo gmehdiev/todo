8eb6ac1ead875e9b547342a05e0a074b8918cf14
gosha <gmehdiev@gmail.com>
2023-04-02 15:53:36 +0300

    ploho

--- script.js
+++ script.js
@@ -5,32 +5,54 @@ const sendNote = document.querySelector('.add')
 
 const wrapper = document.querySelector('.wrapper')
 
+const title = document.querySelector('input')
+const descr = document.querySelector('textarea')
+
+const del = document.querySelector('.del');
+
+const element = document.getElementById("1");
+
+
 createNote.addEventListener("click", () => addNote.style.display = 'flex')
 closeNote.addEventListener("click", () => addNote.style.display = 'none')
 
+let noteCount = 1;
 sendNote.addEventListener("click",asd)
 // sendNote.addEventListener("click", () =>   addNote.style.display = 'none')
 
 
+
+
+function ads() {
+    document.getElementById("del"+del.id).remove();
+
+
+}
+
 function asd() {
+    let date = new Date;
+    
     addNote.style.display = 'none'
-    wrapper.innerHTML +=`        <div class="task">
+    wrapper.innerHTML +=`        <div class="task" id="${"del"+noteCount}">
             <div class="taskName">
-                Lorem, ipsum dolor.
+                ${title.value}
             </div>
             <div class="taskText">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus sed suscipit nesciunt perspiciatis ullam reprehenderit et adipisci eum nemo officiis omnis qui, aut illum, impedit voluptates voluptate nulla incidunt reiciendis, deserunt atque eius ab est! Eius reprehenderit mollitia magni autem eligendi quaerat tempora nobis ducimus ipsum rem corporis sit quasi quisquam soluta, laborum aliquam voluptatibus maxime quo, quidem ipsa deserunt culpa. Laboriosam illum sequi omnis odit rerum cumque, reiciendis vitae hic nemo eius, tenetur consectetur. Beatae recusandae vero perspiciatis commodi, sed molestiae quis! Maxime necessitatibus inventore corporis culpa, similique velit aliquam quo, voluptates quis distinctio aliquid blanditiis consectetur sed placeat.
+            ${descr.value}
             </div>
             <div class="toolbar">
-                <div class="delete">
-                    удалить
+                <div class="del" id="${noteCount}">
+                    
                 </div>
                 <div class="date">
-                    дата
+                    ${date.toLocaleDateString()}
                 </div>
             </div>
         </div>`;
+        noteCount++;
 }
 // document.body.innerHTML = ''
 
+
 createNote.addEventListener("click", () => addNote.style.display = 'flex')
+del.addEventListener("click", ()=>ads())
